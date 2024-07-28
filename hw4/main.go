98193cddafe02b8d6c74326d535d4e5b9e2510cd
KAYURO25182212 <153671839+KAYURO25182212@users.noreply.github.com>
2024-07-29 01:39:30 +0500

    fourth

---
+++ hw4/main.go
@@ -0,0 +1,43 @@
+package main
+
+func main() {
+	//zadacha 1
+	/*for i := 1; i <= 10; i++ {
+		println(i)
+
+	}
+	//zadacha 2
+	println("--------------")
+	for i := 1; i <= 5; i++ {
+		fmt.Printf("%d * %d = %d\n", i, i, i*i)
+	}*/
+	//zadacha 3
+	/*println("--------------")
+	for i := 1; i <= 10; i++ {
+		n := 3
+		fmt.Printf("%d * %d = %d\n", i, n, i*n)
+	}
+	println("--------------")*/
+	//zadacha 4
+	/*var a int = 0
+	var b int = 1
+	for i := 0; i <= 10; i++ {
+		x := a
+		a = b
+		b = a + b
+		println(x)*/
+	//zadacha 5
+	/*var a int = 256
+	var b int = 512
+	for b != 0 {
+		a %= b
+		if a == 0 {
+			println(b)
+		}
+		b %= a
+		println(a)
+	}*/
+	//facktorial
+	
+
+}
